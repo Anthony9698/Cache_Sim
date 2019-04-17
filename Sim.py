a06bf7265e351f0dceab215fb02daa13829e9d2e
Anthony <anthonyviera4@gmail.com>
2019-04-17 13:22:24 -0500

    Finished implementation with RR

--- Sim.py
+++ Sim.py
@@ -1,46 +1,29 @@
 #!/usr/bin/python3
-# Example run: ./Sim.py -f TinyTrace.trc -s 1024 -b 16 -a 2 -r RR
-
-# Sample Run Command: ./Sim.py -f Trace2A.trc -s 1024 -b 16 -a 2 -r RR
+# Currently Running as: ./Sim.py -f Trace2A.trc -s 2048 -b 16 -a 2 -r RR
 
 import sys
 import math
 
-
 cacheSz = int(sys.argv[4])
 blockSz = int(sys.argv[6])
 associativity = int(sys.argv[8])
-offset = math.log(blockSz, 2)
+offsetBits = int(math.log(blockSz, 2))
 
-# TODO(1): implement RR, RND, and LRU(bonus if we can)
-#       replacement policies
-
-
-# TODO(2): calculate...
-#                   > Total Cache Accesses (hits + misses)
-#                   > Cache Hits
-#                   > Cache Misses
-#                   > Compulsory Misses
-#                   > Conflict Misses
+miss_count = 0
+hit_count = 0
+compulsory_count = 0
+conflict_count = 0
 
 
-# TODO(3): create appropriate arrays (multidimensional if assoc > 1)
-#       to set up cache using this object.
-#       Example: CacheSize: 4096, ASSOC: 2
-#       CacheBlock cache[4096][2]
-#                         ^    ^
-#                      INDEX# SET#
 # represents a block in a cache
 class CacheBlock:
     'Common base class for all cache blocks'
-    hit_count = 0
-    miss_count = 0
 
     # cache block constructor
-    def __init__(self, hex_index=0, hex_tag=0, hex_data=0):
-        self.hex_index = hex_index
+    def __init__(self):
         self.vi_bit = 0
-        self.hex_index = hex_tag
+        self.hex_tag = 0x0
+        self.clock = 0
 
     # returns information about object (toString)
     # example print(cache_block)
@@ -77,32 +60,24 @@ def hex_to_binary_str(hex_str):
     return binary_str
 
 
-# TODO(4): use tag and index info returned from this
-#       method to create cache block objects
-def get_hex_for_tag_index(src_b, tag_bits, index_bits):
-    tag_bit_str = ''
-    index_bit_str = ''
+def convert_to_hex_tag(bin_addr_str):
+    global tagBits
+    start = 0
+    end = tagBits
+    hex_tag = bin_addr_str[start:end]
+    hex_tag = hex(int(hex_tag, 2))
 
-    # prints instruction address in binary (for debugging)
-    print(src_b)
+    return hex_tag
 
-    # sections bits for tag and index
-    for bit in src_b:
-        if len(tag_bit_str) != tag_bits:
-            tag_bit_str += bit
 
-        elif len(index_bit_str) != index_bits:
-            index_bit_str += bit
+def convert_to_hex_index(bin_addr_str):
+    global tagBits, indexBits
+    start = tagBits
+    end = tagBits + indexBits
+    index = bin_addr_str[start:end]
+    index = int(index, 2)
 
-    tag_hex = hex(int(tag_bit_str, 2))
-    index_hex = hex(int(index_bit_str, 2))
-
-    # printing index and tag values in hex (for debugging)
-    print("INDEX: ", index_hex)
-    print("TAG: ", tag_hex)
-    print("=============================")
-
-    #return tag_hex, index_hex
+    return index
 
 
 def calcIndexBits(assocBits, cacheBits, offset):
@@ -164,23 +139,166 @@ def calcTotalImplementationSz(totalOverhead, cacheBits):
     return total_imp_sz
 
 
-def next_avail_col(cache, hex_index):
-    index = 0
-    int_index = int(hex_index, 10)
-    for blk in cache[int_index]:
+# cache miss, tag not set
+def next_avail_col_blk(cache_obj, index):
+    global compulsory_count
+    col_ndx = 0
+    for blk in cache_obj[index]:
         if blk.vi_bit == 0:
-            return index
+            compulsory_count += 1
+            avail_col = col_ndx
+            return avail_col
         else:
-            index += 1
-    return index
+            col_ndx += 1
+
+    return -1
+
+
+def check_if_cache_empty(cache_obj, index):
+    for blk in cache_obj[index]:
+        if blk.vi_bit != 0:
+            return False
+
+    return True
+
+
+def tags_match(cache_obj, index, tag):
+    global miss_count
+    global hit_count
+    for blk in cache_obj[index]:
+        if blk.vi_bit == 1 and tag == blk.hex_tag:
+            hit_count += 1
+            return True
+
+    # didn't find a matching tag or valid bit
+    # was not set
+    miss_count += 1
+    return False
+
+
+def rr_block_replacement(cache_obj, index):
+    lowest_clk = sys.maxsize
+    highest_clk = - sys.maxsize - 1
+    high_ndx = -1
+    low_ndx = -1
+    loop_count = 0
+    for blk in cache_obj[index]:
+        if lowest_clk > blk.clock:
+            lowest_clk = blk.clock
+            low_ndx = loop_count
+
+        loop_count += 1
+
+    loop_count = 0
+    for blk in cache_obj[index]:
+        if highest_clk < blk.clock:
+            highest_clk = blk.clock
+            high_ndx = loop_count
+
+        loop_count += 1
+
+    return low_ndx, high_ndx
+
+
+def update_cache(cache, ndx, tag):
+    global miss_count, conflict_count, compulsory_count
+
+    # every block in set has a vi bit set to zero
+    if check_if_cache_empty(cache, ndx) is True:
+        compulsory_count += 1
+        miss_count += 1
+        col = 0
+        cache[ndx][col].vi_bit = 1
+        cache[ndx][col].hex_tag = tag
+        cache[ndx][col].hex_index = hex(ndx)
+
+    # check if current tag matches any block in the blk set at cache[int_ndx]
+    elif tags_match(cache, ndx, tag) is False:
+        col = next_avail_col_blk(cache, ndx)
+
+        # set is full at cache[int_ndx], find blk to replace with RR
+        if col == -1:
+            conflict_count += 1
+            low, high = rr_block_replacement(cache, ndx)
+            high_clk = cache[ndx][high].clock
+            cache[ndx][low].vi_bit = 1
+            cache[ndx][low].hex_tag = tag
+            cache[ndx][low].clock = high_clk + 1
+            cache[ndx][low].hex_index = hex(ndx)
+
+        else:
+            cache[ndx][col].vi_bit = 1
+            cache[ndx][col].hex_tag = tag
+            cache[ndx][col].hex_index = hex(ndx)
+
+    # return updated cache
+    return cache
+
+
+# appends zero char to hex address if it needs it
+def append_zero_char(hex_address):
+    if len(hex_address) < 8:
+        hex_address = '0' + hex_address
+
+    return hex_address
+
+
+def return_second_index_address(old_address, bytes_read):
+    new_addr_h = int(old_address, 16) + bytes_read
+    new_addr_h = str(hex(new_addr_h))[2:]
+    new_addr_h = append_zero_char(new_addr_h)
+
+    return new_addr_h
+
+
+def format_hex_num(unformatted_hex):
+    while(len(unformatted_hex) != 8):
+        unformatted_hex = '0' + unformatted_hex
+
+    return unformatted_hex
+
+
+def access_cache(hex_address, bytes_read):
+    global cache, blockSz
+
+    binary_addr_str = hex_to_binary_str(hex_address)
+    hex_tag = convert_to_hex_tag(binary_addr_str)
+    index = convert_to_hex_index(binary_addr_str)
+    cache = update_cache(cache, index, hex_tag)
+
+    # string of binary offset
+    offset_binary_nums = binary_addr_str[-offsetBits:]
+
+    # add bytes read to this binary offset
+    binary_offset = int(offset_binary_nums, 2) + bytes_read
+
+    ''' if binary offset after add is more than block size
+        find out how many cache blocks it accesses'''
+    if binary_offset > blockSz:
+        access_count = int(binary_offset / blockSz)
+        count = 0
+
+        ''' while there are more cache blocks to access'''
+        while count < access_count:
+            start = 0
+            end = tagBits
+            hex_tag = hex(int(binary_addr_str[start:end], 2))
+
+            start = tagBits
+            end = tagBits+indexBits
+            str_index = binary_addr_str[start:end]
+            index = int(str_index, 2) + 1
+
+            cache = update_cache(cache, index, hex_tag)
+            count += 1
 
 
 cacheBits = int(math.log(cacheSz, 2)) + 10
 assocBits = int(math.log(associativity, 2))
-indexBits = calcIndexBits(assocBits, cacheBits, offset)
+indexBits = calcIndexBits(assocBits, cacheBits, offsetBits)
 totalIndices = calcTotalIndices(indexBits)
 totalIndicesSuffix = calcSuffix(indexBits)
-tagBits = int(32 - (offset + indexBits))
+tagBits = 32 - (offsetBits + indexBits)
 totalBlocks = calcTotalBlocks(assocBits, indexBits)
 totalBlocksBits = int(assocBits + indexBits)
 totalBlocksSuffix = calcSuffix(totalBlocksBits)
@@ -211,13 +329,20 @@ print('Tag Size:', tagBits, 'bits')
 print('Index Size:', indexBits, 'bits, Total Indices:', totalIndices, totalIndicesSuffix)
 print('Overhead:', totalOverSz_str, 'bytes (or ' + totalOverheadSz_kb_str + ' KB)')
 print('Total Implementation Memory Size:', totalImpSz_str, 'bytes (or ' + totalImpSz_kb_str + ' KB)')
-print('-----Results-----')
-print('Cache Hit Rate:%')
+
 
 # setting up cache
-rows = int(math.pow(indexBits, 2))
+rows = int(math.pow(2, indexBits))
 cols = int(associativity)
 cache = [[CacheBlock() for j in range(cols)] for i in range(rows)]
+
+
+# making blocks in a set a linked list
+for row in cache:
+    count = 0
+    for col in row:
+        col.clock = count
+        count += 1
 
 # parsing
 file_name = sys.argv[2]
@@ -226,7 +351,7 @@ try:
 except FileNotFoundError:
     raise FileNotFoundError("Input file does not exist")
 
-non_zero_count = 0
+# for every eip and dstm instruction
 for line in input_file:
     # strip newline and tab characters from every line
     line = line.strip('\n')
@@ -241,19 +366,38 @@ for line in input_file:
     line_prefix = ''.join(line_tokens[0:3])
 
     if line_prefix == 'EIP':
-        bytes_read = ''.join(line_tokens[5:7])
+        bytes_read = int(''.join(line_tokens[5:7]))
         instr_addr_str = ''.join(line_tokens[10:18])
 
+        # access cache at this address
+        access_cache(instr_addr_str, bytes_read)
+
     if line_prefix == 'dst':
+        # parsing out dstM and srcM from line
         dstM = ''.join(line_tokens[6:14])
         srcM = ''.join(line_tokens[33:41])
 
-        # sets string instruction address to binary string
-        # example: 0x7C81EB33 = 01111100100000011110101100110011
-        instr_addr_str_b = hex_to_binary_str(instr_addr_str)
+        # assume 4 byte read and writes
+        bytes_read = 4
 
-        # print current instruction address (testing for now)
-        print("Instruction Address: ", instr_addr_str)
+        # check if dstM address is in the cache
+        if dstM != '00000000':
+            access_cache(dstM, bytes_read)
 
-        # prints hex tag and hex index
-        get_hex_for_tag_index(instr_addr_str_b, tagBits, indexBits)
+        # check if srcM address is in the cache
+        if srcM != '00000000':
+            access_cache(srcM, bytes_read)
+
+
+total_cache_accesses = hit_count + miss_count
+miss_rate = miss_count / total_cache_accesses
+miss_rate = '{:.4%}'.format(miss_rate)
+
+print("***** Cache Simulation Results *****\n")
+print("Total Cache Accesses:   ", total_cache_accesses)
+print("Cache Hits:\t\t", hit_count)
+print("Cache Misses:\t\t", miss_count)
+print("--- Compulsory Misses:\t   ", compulsory_count)
+print("--- Conflict Misses:\t   ", conflict_count, "\n\n")
+print("***** *****  CACHE MISS RATE:  ***** *****\n")
+print("Miss Rate = ", miss_rate)
