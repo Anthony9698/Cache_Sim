b2081d834753e824276fe9fa1f4dbf95b4c1d221
Anthony <anthonyviera4@gmail.com>
2019-04-19 09:39:13 -0500

    modified script

--- TableWriter.py
+++ TableWriter.py
@@ -1,12 +1,49 @@
 #!/usr/bin/python3
 
-#from openpyxl import Workbook
+from openpyxl import Workbook
 import sys
 import re
-data = sys.stdin.readlines()
+
+if len(sys.argv) < 2:
+    print("--- Input file required ---")
+    sys.exit(-1)
+
+
+# create Workbook object
+wb = Workbook()
+
+# set file path
+file_path = "/home/anthony/PycharmProjects/Cache_Sim/demo.xlsx"
+
+# select demo.xlsx
+sheet = wb.active
+
+data = [('Trace File',
+         "Cache Size",
+         "Block Size",
+         "Assoc",
+         "R-Policy",
+         "Total Blocks",
+         "Tag Bits",
+         "Index Bits",
+         "Total Rows",
+         "Total CA",
+         "Hits",
+         "Misses",
+         "Comp Misses",
+         "Conflict Misses",
+         "Miss Rate %")]
+
+
+file_name = sys.argv[1]
+try:
+    input_file = open(file_name, "r")
+except FileNotFoundError:
+    raise FileNotFoundError("Input file does not exist")
 
 line_count = 1
-for line in data:
+new_row = []
+for line in input_file:
     # strip newline and tab characters from every line
     line = line.strip('\n')
     line = line.strip('\t')
@@ -23,35 +60,41 @@ for line in data:
 
     # Trace file line
     if line_count is 3:
-        print(f_line)
+        new_row = []
+        trace_file = f_line
+        new_row.append(trace_file)
 
     # cache size
     elif line_count is 5:
-        print(f_line)
+        cache_size = f_line
+        new_row.append(cache_size)
 
     # block size
     elif line_count is 6:
         block_size = f_line.split("bytes")
-        block_size = f_line[0] + "B"
-        print(block_size)
+        block_size = block_size[0] + "B"
+        new_row.append(block_size)
 
     # assoc
     elif line_count is 7:
-        print(f_line)
+        assoc = f_line
+        new_row.append(assoc)
 
     # R-Policy
     elif line_count is 8:
-        print(f_line)
+        r_policy = f_line
+        new_row.append(r_policy)
 
     # total blocks
     elif line_count is 10:
         total_blocks = f_line.split("(")
         total_blocks = total_blocks[0]
-        print(total_blocks)
+        new_row.append(total_blocks)
 
     # tag bits
     elif line_count is 11:
-        print(f_line)
+        tag_bits = f_line
+        new_row.append(tag_bits)
 
     # index bits and total rows
     elif line_count is 12:
@@ -59,64 +102,48 @@ for line in data:
         index_bits = index_line_elements[0]
         index_line_elements = f_line.split(":")
         total_indices = index_line_elements[1]
-
-        print(index_bits)
-        print(total_indices)
+        new_row.append(index_bits)
+        new_row.append(total_indices)
 
     # total cache accesses
     elif line_count is 17:
-        print(f_line)
+        total_CA = f_line
+        new_row.append(total_CA)
 
     # Hits
     elif line_count is 18:
-        print(f_line)
+        hits = f_line
+        new_row.append(hits)
 
     # Misses
     elif line_count is 19:
-        print(f_line)
+        misses = f_line
+        new_row.append(misses)
 
     # Compulsory misses
     elif line_count is 20:
-        print(f_line)
+        comp_misses = f_line
+        new_row.append(comp_misses)
 
-    # conflict misses
+    # Conflict misses
     elif line_count is 21:
-        print(f_line)
+        conflict_misses = f_line
+        new_row.append(conflict_misses)
+
+    # Miss rate
+    elif line_count is 26:
+        miss_rate = line.split("=")
+        miss_rate = miss_rate[1]
+        new_row.append(miss_rate)
 
     line_count += 1
 
-''''
-# create Workbook object
-wb = Workbook()
+    if line_count == 27:
+        data.append(new_row)
+        line_count = 1
 
-# set file path
-file_path = "/home/anthony/PycharmProjects/Cache_Sim/demo.xlsx"
-
-# select demo.xlsx
-sheet = wb.active
-
-data = [('Trace File',
-         "Cache Size",
-         "Block Size",
-         "Assoc",
-         "R-Policy",
-         "Tag Bits",
-         "Index Bits",
-         "Hits",
-         "Misses",
-         "Miss Rate %")]
-
-
-# append all rows
+# write all rows to table
 for row in data:
     sheet.append(row)
 
-r = 10
-c = 11
-
-sheet.cell(row=r, column=c).value = "FUCK"
-
-# save file
 wb.save(file_path)
-
-'''
--- runner.py
+++ runner.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 
 import os
-import time
-
 
 cache_size = 1
-block_size = 2
+block_size = 4
 assoc = 1
 
 while assoc != 16:
 
-    if cache_size == 128 and block_size == 64:
+    if cache_size > 8192 or block_size > 64 or assoc > 16:
+        print("CACHE SIZE: ", cache_size)
+        print("BLOCK SIZE: ", block_size)
+        print("ASSOC: ", assoc)
+        exit(-1)
+
+    if cache_size == 8192 and block_size == 64:
         cache_size = 1
         block_size = 4
         assoc *= 2
 
-    elif cache_size == 128:
+    elif cache_size == 8192:
         block_size *= 2
         cache_size = 1
 
-    command = './Sim.py -f Trace2A.trc -s ', str(cache_size), " -b ", str(block_size), ' -a ', str(assoc), ' -r RR'
+    command = './Sim.py -f Trace1.trc -s ', str(cache_size), " -b ", str(block_size), ' -a ', str(assoc), ' -r RR'
     command = ''.join(command)
     os.system(command)
 
     cache_size *= 2
 
-    print("Cache Size: ", cache_size, " Block size: ", block_size, " Assoc: ", assoc)
-    time.sleep(8)
+
 
